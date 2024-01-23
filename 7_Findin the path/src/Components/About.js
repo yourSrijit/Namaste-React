cca54e27de19b380ea23664c140d138af1ba2fff
Srijit Bera <berasrijit02@gmail.com>
2024-01-23 14:55:55 +0530

    Delete 7_Findin the path directory

--- 7_Findin the path/src/Components/About.js
+++
@@ -1,9 +0,0 @@
-const About=()=>{
-    return(
-        <div>
-        <h1>About Us</h1>
-        <h2>Hi iam srijit</h2>
-        </div>
-    ); 
-}
-export default About;
--- 7_Findin the path/src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const CDN_URL=  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-export const LOGO_URL= "https://i.pinimg.com/originals/59/5f/a5/595fa5ffb3d67f62ceabaa0d9a40d1e2.png";
