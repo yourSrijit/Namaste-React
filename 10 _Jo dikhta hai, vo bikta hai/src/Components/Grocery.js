2ace4923d175c8100f6b5c421de44c19e6f5c14a
Srijit Bera <berasrijit02@gmail.com>
2024-03-07 21:50:50 +0530

    Delete 10 _Jo dikhta hai, vo bikta hai directory

--- 10 _Jo dikhta hai, vo bikta hai/src/Components/Grocery.js
+++
@@ -1,9 +0,0 @@
-const Grocery=()=>{
-  return(
-    <div>
-        <h1>This is Grocery (Example of Lazy loading/on demand loading/chunking/code spilliting/dunamic bundling)</h1>
-    </div>
-  )
- 
-}
-export default Grocery;
--- 10 _Jo dikhta hai, vo bikta hai/src/Components/Shimmer.js
+++
@@ -1,8 +0,0 @@
-const Shimmer=()=>{
-    return <div className="shimmer-container">
-        <div className="shimmer-card">Cards</div>
-        <div className="shimmer-card">Cards</div>
-        <div className="shimmer-card">Cards</div>
-    </div>
-}
-export default Shimmer;
