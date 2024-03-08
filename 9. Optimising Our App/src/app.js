2ba538f2c5a53d261faae2ce69d781a6c39d7aae
Srijit Bera <berasrijit02@gmail.com>
2024-03-08 20:09:45 +0530

    Add files via upload

---
+++ 9. Optimising Our App/src/app.js
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import { Body } from "./components/Body";
+import UserContextProvider from "./context/userContextProvider";
+import Footer from "./components/Footer.js";
+import { createBrowserRouter,RouterProvider ,Outlet} from "react-router-dom";
+import About from "./components/About.js";
+import { Auth0Provider } from '@auth0/auth0-react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
+import RestaurantMenu from "./components/RestaurantMenu.js";
+
+
+const AppLayout=()=>{
+    return(
+        <Auth0Provider
+        domain="dev-kw1jgnrfpswitzip.us.auth0.com"
+        clientId="F71ONJJk6YI37U58ucX4Bs5tCkuXTtVk"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}>
+        <UserContextProvider>
+        <div className="app">
+        <Header/>
+        <Outlet/>
+        <Footer/>
+        </div>
+        </UserContextProvider>
+        <ToastContainer/>
+        </Auth0Provider>
+    )
+}
+const appRouter=createBrowserRouter([
+    {
+    path:'/',
+    element:<AppLayout/>,
+    children:[
+        {
+            path:'/',
+            element:<Body/>
+        },
+        {
+            path:'/about',
+            element:<About/>
+        },
+        {
+            path:'/contact',
+            element:<Contact/>
+        },
+        {
+            path:'/restaurant/:resId',
+            element:<RestaurantMenu/>
+        },
+    ],
+    errorElement:<Error/>
+    },
+    
+
+])
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render( <RouterProvider router={appRouter}/>);
